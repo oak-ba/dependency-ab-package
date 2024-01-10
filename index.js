ba0ef40ab5141355a6aa3e8d2ccd5731c5ab1a64
kaoutarBa <baldi.kaoutar.ma@gmail.com>
2024-01-10 16:10:21 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+//Hello, This a test for my scenario on CI/CD hacking lab
+function dependencyAb() {
+    return "Bonjour, cette version est la bonne 1.0.0"
+  }
+  
+  module.exports = dependencyAb
